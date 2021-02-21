80fc54e76a7ed191be78ae2a8f9818c9c86e953b
Rashid Reyaz <rreyaz14@in.ibm.com>
2021-02-21 14:48:24 +0530

    Update to this simple calc

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+// Set value of Range slider
+function displayRange(){
+    document.getElementById("range_val").innerHTML=rate.value;
 }
-        
+
+// Compute interest
+function compute()
+{   
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+
+    // Validataion for Principal to be a positive number
+    if(principal.value<=0) {
+        alert("Enter a positive number");
+        principal.focus();
+    }
+    else {
+        curr_year=new Date().getFullYear();
+        interest = (principal * years * rate) / 100;
+        document.getElementById("result").innerHTML='If you deposit <mark>'+principal+ '</mark>, <br>' 
+        +' at an interest rate of <mark>'+rate+'%</mark>. <br>' 
+        +' You will receive an amount of <mark>' +interest+'</mark>, <br>'
+        +' in the year <mark>' +(curr_year+Number(years))+'</mark>'
+    }
+}
